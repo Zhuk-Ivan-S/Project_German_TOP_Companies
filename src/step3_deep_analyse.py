757b6096c653abcfeee48cad03dab09f69d6daf6
Zhuk-Ivan-S <zhukivanserhiyovich@gmail.com>
2025-08-22 13:22:49 +0200

    InitCommit

--- src/step3_deep_analyse.py
+++ src/step3_deep_analyse.py
@@ -79,3 +79,45 @@ plt.grid(True, axis='y', linestyle='--', alpha=0.6, color='gray')
 plt.tight_layout()
 plt.legend(bbox_to_anchor=(0.1, 0.51))
 plt.show()
+
+#Try to show prediction for Net Income in next year for Companies (Linear Regression) - the same method
+# just change column from Revenue to Net Income (algorithm is the same)
+df['Year'] = pd.to_datetime(df['Period']).dt.year
+#create list for prediction
+prediction = []
+for company in df['Company'].unique():
+    company_df = df[df['Company']==company].sort_values('Year')
+    X = company_df['Year'].values.reshape(-1,1)
+    y = company_df['Net Income'].values
+    #model creating
+    model = LinearRegression()
+    model.fit(X,y)
+    next_year = np.array([[X.max() + 1]])
+    pred = model.predict(next_year)[0]
+    prediction.append({'Company': company,'Predicted_Net_Income_next_year':pred})
+
+pred_df = pd.DataFrame(prediction).sort_values(by='Predicted_Net_Income_next_year', ascending=False)
+print(pred_df.head(12))
+
+#Show a prediction for each company with line plot
+
+plt.figure(figsize=(15,6))
+companies = df['Company'].unique()
+colors = sns.color_palette('tab10', n_colors=len(companies))
+
+for company in companies:
+    numeric_cols = ['Revenue', 'Net Income', 'Liabilities', 'Assets', 'Equity', 'ROA (%)', 'ROE (%)', 'Debt to Equity']
+    company_df = df[df['Company'] == company].groupby('Year')[numeric_cols].mean().reset_index().sort_values('Year')
+    plt.plot(company_df['Year'], company_df['Net Income'], marker='o', label=company)
+    next_year = company_df['Year'].max() + 1
+    pred = pred_df.loc[pred_df['Company'] == company, 'Predicted_Net_Income_next_year'].values[0]
+    plt.scatter(next_year, pred, marker='X', s=100)
+
+plt.xlabel('Year')
+plt.ylabel('Net Income')
+plt.title('Revenue and predict for next year')
+
+plt.grid(True, axis='y', linestyle='--', alpha=0.6, color='gray')
+plt.tight_layout()
+plt.legend(bbox_to_anchor=(0.1, 0.51))
+plt.show()
